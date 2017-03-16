7861bb945108422892f9dc1d1cd630472ff77f92
l16426434 <l16426434@gmail.com>
2017-03-16 13:44:21 +0800

    0.1
    0.1

---
+++ app/src/main/java/com/liujishun/library/MainActivity.java
@@ -0,0 +1,11 @@
+package com.liujishun.library;
+
+public class MainActivity extends BaseActivity {
+
+  
+
+    @Override
+    protected int getContentView() {
+        return R.layout.activity_content;
+    }
+}
---
+++ library/src/main/java/com/test/library/manager/OnRetryListener.java
@@ -0,0 +1,9 @@
+package com.test.library.manager;
+
+/**
+ * Created by chenpengfei on 2017/2/6.
+ */
+public interface OnRetryListener {
+
+    void onRetry();
+}
